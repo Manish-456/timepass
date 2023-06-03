392f94c890f83811b251bed3e4929d1b9d872437
Manish-456 <tamangmanish3456@gmail.com>
2023-06-03 12:18:16 +0545

    small bug fixed

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,7 @@ import getContents from "./getContents.js";
     const para = document.querySelector('p');
     const content = document.getElementById("content");
     const ul = document.getElementById("ul");
+    const warn = document.querySelector(".warn");
     const players = []
     let playerCount = 0;
     function updateName() {
@@ -11,22 +12,19 @@ import getContents from "./getContents.js";
       players.push(name);
       alert.textContent = `New Player added successfully`;
       playerCount++;
-
+     
       para.innerHTML = `player ${playerCount}: ${name}`
       alert.style.opacity = 1;
-
+      warn.style.display = "none"  
       if (players.length > 0) {
-
-        let li = document.createElement("li");
-
-        players.forEach(player => {
-          li.textContent = `${player} `
-        })
-
+          let li = document.createElement("li");
+          
+          players.forEach(player => {
+              li.textContent = `${player} `
+            })
         ul.appendChild(li);
-      } else {
-        ul.innerHTML = "No players has been added yet"
-      }
+  
+    } 
       setTimeout(() => {
         alert.style.opacity = 0
       }, 5000)
