eae9c7f5fd93093aed338c1ee4b1289c9a3bf848
Manish-456 <tamangmanish3456@gmail.com>
2023-06-03 12:20:02 +0545

    final commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,53 +1,45 @@
-
 import getContents from "./getContents.js";
-    const alert = document.getElementById("alert");
-    const para = document.querySelector('p');
-    const content = document.getElementById("content");
-    const ul = document.getElementById("ul");
-    const warn = document.querySelector(".warn");
-    const players = []
-    let playerCount = 0;
-    function updateName() {
-      const name = prompt("Enter new name");
-      players.push(name);
-      alert.textContent = `New Player added successfully`;
-      playerCount++;
-     
-      para.innerHTML = `player ${playerCount}: ${name}`
-      alert.style.opacity = 1;
-      warn.style.display = "none"  
-      if (players.length > 0) {
-          let li = document.createElement("li");
-          
-          players.forEach(player => {
-              li.textContent = `${player} `
-            })
-        ul.appendChild(li);
-  
-    } 
-      setTimeout(() => {
-        alert.style.opacity = 0
-      }, 5000)
-    }
-    para.addEventListener('click', updateName);
 
-    // Add change event on select element
+const alert = document.getElementById("alert");
+const para = document.querySelector("p");
+const content = document.getElementById("content");
+const ul = document.getElementById("ul");
+const warn = document.querySelector(".warn");
+const players = [];
+let playerCount = 0;
 
-    const opt = document.getElementById("weather");
+function updateName() {
+  const name = prompt("Enter new name");
+  players.push(name);
+  alert.textContent = `New Player added successfully`;
+  playerCount++;
 
-    opt.addEventListener("change", setWeather);
+  para.innerHTML = `player ${playerCount}: ${name}`;
+  alert.style.opacity = 1;
+  warn.style.display = "none";
+  if (players.length > 0) {
+    let li = document.createElement("li");
 
-    function setWeather() {
-      const { value } = opt;
-      const contentLists = getContents(value);
+    players.forEach((player) => {
+      li.textContent = `${player} `;
+    });
+    ul.appendChild(li);
+  }
+  setTimeout(() => {
+    alert.style.opacity = 0;
+  }, 5000);
+}
+para.addEventListener("click", updateName);
 
+// Add change event on select element
 
-      content.textContent = contentLists.desc;
+const opt = document.getElementById("weather");
 
+opt.addEventListener("change", setWeather);
 
+function setWeather() {
+  const { value } = opt;
+  const contentLists = getContents(value);
 
-
-
-
-
-    }
+  content.textContent = contentLists.desc;
+}
